9462ce52210cfdef106cee5dcaa6f02149f2b1fc
mayanknaughtiyal <nautiyalmamta198@gmail.com>
2024-09-25 12:58:03 +0530

    Create python code.py

---
+++ python code.py
@@ -0,0 +1,15 @@
+import pandas as pandas
+import keyword
+from datetime import datetime
+import operator
+print(keyword.kwlist)
+'''#identifiers cannot be keywords  : they can end with numbers but cannot start with numbers , they cannot use special characters.
+comments are just for coder purpose not for computer they can start with # and end with . or they can start with ''' '''
+in python one can write statements in a single line or multiple lines
+for eg '''
+sum = 2+2+555+2
+print(sum)
+#or.
+sum = 2+22\
++22 +26
+print(sum)
